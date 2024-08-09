be4335bca0deebbf546855351f43f308b8ec2a6e
Marcos Fernandes <100423775+marckfernandes@users.noreply.github.com>
2024-08-08 22:26:44 -0300

    tentando fazer o git ignorar a pasta nodemodules

--- src/Componentes/Icones/IconeSvg.js
+++ src/Componentes/Icones/IconeSvg.js
@@ -6,13 +6,10 @@ const IconeSvg = ({paths, fill="#0072D8", className}) => {
             viewBox="0 0 32 32"
             width="32"
             height="32"
-            fill={fill} 
-            {/*Cor padrão do ícone*/}
-            className={className}
-            {/*Permite sobrescrever a cor com CSS adicionando className */}
-        >
-            {paths.map((path, index) => (
-            {/*Esse map cria um path para cada item dentro do array de paths do svg*/}
+            fill={fill /*Cor padrão do ícone*/} 
+            className={className /*Permite sobrescrever a cor com CSS adicionando className */}
+            >
+            {paths.map((path, index) => ( /*Esse map cria um path para cada item dentro do array de paths do svg*/
                 <path fill-rule="evenodd" clip-rule="evenodd" key={index} d={path} />
             ))}
         </svg>
